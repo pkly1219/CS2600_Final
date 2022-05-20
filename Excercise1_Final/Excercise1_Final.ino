0dd8c51be72495afceb940a1fd4732c93e23dfba
Phung Ly <98497119+pkly1219@users.noreply.github.com>
2022-05-20 16:52:44 -0700

    Update Excercise1_Final.ino

--- Excercise1_Final/Excercise1_Final.ino
+++ Excercise1_Final/Excercise1_Final.ino
@@ -2,9 +2,8 @@
 #include <PubSubClient.h>
 
 // WiFi
-const char *ssid = "Baby"; // Enter your WiFi name
-const char *password = "LuvU";  // Enter WiFi password
-
+const char* ssid = "Xai3GDi";
+const char* password = "khongbiet";
 // MQTT Broker
 const char *mqtt_broker = "192.168.3.45";
 const char *topic = "esp32/test";
@@ -12,25 +11,39 @@ const char *mqtt_username = "emqx";
 const char *mqtt_password = "123456789";
 const int mqtt_port = 1883;
 
-WiFiClient espClient;
-PubSubClient client(espClient);
+#define LEDPIN 2
 
-int led = 2;
-int entry = 0;
+unsigned long previousMillis = 0; 
+const long interval = 5000;
+int current_ledState = LOW;
+int last_ledState = LOW;
 
-void setup() {
- // Set software serial baud to 115200;
- Serial.begin(115200);
- // connecting to a WiFi network
- WiFi.begin(ssid, password);
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+
+void setup_led()
+{
+  //initialize digital pin PIN_LED as an output
+  pinMode(LEDPIN,OUTPUT);
+  pinMode(current_ledState,INPUT);
+}
+
+void setup_wifi() {
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+   WiFi.begin(ssid, password);
  while (WiFi.status() != WL_CONNECTED) {
      delay(500);
      Serial.println("Connecting to WiFi..");
  }
  Serial.println("Connected to the WiFi network");
- //connecting to a mqtt broker
- client.setServer(mqtt_broker, mqtt_port);
- client.setCallback(callback);
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void connect_to_broker() {
+  client.setServer(mqtt_broker, mqtt_port);
+  client.setCallback(callback);
  while (!client.connected()) {
      String client_id = "esp32-client-";
      client_id += String(WiFi.macAddress());
@@ -43,34 +56,36 @@ void setup() {
          delay(2000);
      }
  }
- 
-
-  pinMode(led, OUTPUT);
-  
+ client.subscribe(topic);
 }
 
-void callback(char *topic, byte *payload, unsigned int length) {
- Serial.print("Message arrived in topic: ");
- Serial.println(topic);
- Serial.print("Message:");
- for (int i = 0; i < length; i++) {
-     Serial.print((char) payload[i]);
- }
- Serial.println();
- Serial.println("-----------------------");
+void callback(char* topic, byte *payload, unsigned int length) {
+  Serial.println("-------new message from broker-----");
+  Serial.print("topic: ");
+  Serial.println(topic);
+  Serial.print("message: ");
+  Serial.write(payload, length);
+  Serial.println();
+  if (*payload == '1') current_ledState = HIGH;
+  if (*payload == '0') current_ledState = LOW;
+}
+void setup() {
+  Serial.begin(115200);
+  Serial.setTimeout(500);
+  setup_wifi();
+  connect_to_broker();
+  setup_led();
 }
 
 void loop() {
- if(Serial.available()>0){ 
- entry = Serial.read();
-  if (entry == '1') {
-    digitalWrite(led, HIGH);    
-    Serial.println("ON");
+ client.loop();
+  if (!client.connected()) {
+    connect_to_broker();
   }
-  else if (entry == '2') {
-   digitalWrite(led, LOW);   
-   Serial.println("OFF");
+  if (last_ledState != current_ledState) {
+    last_ledState = current_ledState;
+    digitalWrite(LEDPIN, current_ledState);
+    Serial.print("LED state is ");
+    Serial.println(current_ledState);
   }
-  
-}
 }
